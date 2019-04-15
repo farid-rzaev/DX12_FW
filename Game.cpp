69402bddaab189a2848181efaa320abeb2027220
Farid Rzaev <farik69rzaev@gmail.com>
2019-04-15 22:37:12 +0300

    Code refactor v3

---
+++ Game.cpp
@@ -0,0 +1,28 @@
+#include "Game.h"
+
+Game::Game(HINSTANCE hInstance, const wchar_t * windowTitle, int width, int height, bool vSync) :
+	Application(hInstance, windowTitle, width, height, vSync),
+	m_ScissorRect(CD3DX12_RECT(0, 0, LONG_MAX, LONG_MAX)),
+	m_Viewport(CD3DX12_VIEWPORT(0.0f, 0.0f, (float)width, (float)height)),
+	m_FoV(45.0f)
+{
+
+}
+Game::~Game() 
+{
+
+}
+
+void Game::Init() {
+	Application::Init();
+}
+
+void Game::Run() {
+	Application::Run();
+}
+
+bool Game::LoadContent() {
+	
+}
+
+
---
+++ Game.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Application.h"
+
+class Game : public Application
+{
+public:
+	Game(HINSTANCE hInstance, const wchar_t * windowTitle, int width, int height, bool vSync);
+	virtual ~Game();
+	
+	virtual void Init();
+	virtual void Run();
+
+private:
+	bool LoadContent();
+	void UnloadContent();
+
+	//void TransitionResource();
+
+private:
+	D3D12_VIEWPORT m_Viewport;
+	D3D12_RECT m_ScissorRect;
+	float m_FoV;
+};
