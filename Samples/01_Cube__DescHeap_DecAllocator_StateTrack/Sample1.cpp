e5e25c909838f46320fbc31d13804f3d3496d293
ferid <farik69rzaev@gmail.com>
2024-09-20 00:37:51 +0100

    Moving Sample01 and Sample02 to separate folders. TODO - create projs.

---
+++ Samples/01_Cube__DescHeap_DecAllocator_StateTrack/Sample1.cpp
@@ -0,0 +1,515 @@
+#include "Sample0.h"
+
+#include <External/Helpers.h>
+
+#include <d3dcompiler.h>
+
+
+// =====================================================================================
+//								   DEFINES / GLOBAL
+// =====================================================================================
+using namespace Microsoft::WRL;
+using namespace DirectX;
+
+// Clamp a value between a min and max range.
+template<typename T>
+constexpr const T& clamp(const T& val, const T& min, const T& max)
+{
+	return val < min ? min : val > max ? max : val;
+}
+
+// Vertex data for a colored cube.
+struct VertexPosColor
+{
+	XMFLOAT3 Position;
+	XMFLOAT3 Color;
+};
+
+static VertexPosColor g_Vertices[8] = {
+	{ XMFLOAT3(-1.0f, -1.0f, -1.0f), XMFLOAT3(0.0f, 0.0f, 0.0f) }, // 0
+	{ XMFLOAT3(-1.0f,  1.0f, -1.0f), XMFLOAT3(0.0f, 1.0f, 0.0f) }, // 1
+	{ XMFLOAT3(1.0f,  1.0f, -1.0f), XMFLOAT3(1.0f, 1.0f, 0.0f) }, // 2
+	{ XMFLOAT3(1.0f, -1.0f, -1.0f), XMFLOAT3(1.0f, 0.0f, 0.0f) }, // 3
+	{ XMFLOAT3(-1.0f, -1.0f,  1.0f), XMFLOAT3(0.0f, 0.0f, 1.0f) }, // 4
+	{ XMFLOAT3(-1.0f,  1.0f,  1.0f), XMFLOAT3(0.0f, 1.0f, 1.0f) }, // 5
+	{ XMFLOAT3(1.0f,  1.0f,  1.0f), XMFLOAT3(1.0f, 1.0f, 1.0f) }, // 6
+	{ XMFLOAT3(1.0f, -1.0f,  1.0f), XMFLOAT3(1.0f, 0.0f, 1.0f) }  // 7
+};
+
+static WORD g_Indicies[36] =
+{
+	0, 1, 2, 0, 2, 3,
+	4, 6, 5, 4, 7, 6,
+	4, 5, 1, 4, 1, 0,
+	3, 2, 6, 3, 6, 7,
+	1, 5, 6, 1, 6, 2,
+	4, 0, 3, 4, 3, 7
+};
+
+
+// =====================================================================================
+//									STATIC - INIT
+// =====================================================================================
+
+
+#if 0
+static Sample0* gs_pSingelton = nullptr;
+
+void Sample0::Create(HINSTANCE hInstance, const wchar_t* windowTitle, int width, int height, bool vSync)
+{
+	if (!gs_pSingelton)
+	{
+		gs_pSingelton = new Sample0(hInstance);
+		gs_pSingelton->Initialize(windowTitle, width, height, vSync);
+	}
+}
+
+Sample0& Sample0::Get()
+{
+	assert(gs_pSingelton);
+	return *gs_pSingelton;
+}
+
+void Sample0::Destroy()
+{
+	if (gs_pSingelton)
+	{
+		//assert(gs_Windows.empty() && gs_WindowByName.empty() &&
+		//	"All windows should be destroyed before destroying the application instance.");
+
+		delete gs_pSingelton;
+		gs_pSingelton = nullptr;
+	}
+}
+#endif
+
+
+// =====================================================================================
+//										Run 
+// =====================================================================================
+
+
+int Sample0::Run() 
+{
+	return Game::Run();
+}
+
+
+// =====================================================================================
+//										Init 
+// =====================================================================================
+
+
+Sample0::Sample0(HINSTANCE hInstance) :
+	Game(hInstance),
+	m_ScissorRect(CD3DX12_RECT(0, 0, LONG_MAX, LONG_MAX)),
+	m_FoV(45.0f)
+{
+}
+
+
+bool Sample0::Initialize(const wchar_t* windowTitle, int width, int height, bool vSync)
+{
+	if (!Game::Initialize(windowTitle, width, height, vSync)) return false;
+
+	// The first back buffer index will very likely be 0, but it depends
+	m_CurrentBackBufferIndex = Application::GetCurrentBackbufferIndex();
+	m_Viewport = CD3DX12_VIEWPORT(0.0f, 0.0f, (float)width, (float)height);
+
+	return true;
+}
+
+
+Sample0::~Sample0()
+{
+
+}
+
+
+// =====================================================================================
+//								LoadContent & UnloadContent
+// =====================================================================================
+
+
+void Sample0::UpdateBufferResource(
+	ComPtr<ID3D12GraphicsCommandList2> commandList,
+	ID3D12Resource** pDestinationResource,
+	ID3D12Resource** pIntermediateResource,
+	size_t numElements, size_t elementSize, const void* bufferData,
+	D3D12_RESOURCE_FLAGS flags)
+{
+	auto device = Application::GetDevice();
+
+	size_t bufferSize = numElements * elementSize;
+
+	// Create a committed resource for the GPU resource in a default heap.
+	ThrowIfFailed(device->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+		D3D12_HEAP_FLAG_NONE,
+		&CD3DX12_RESOURCE_DESC::Buffer(bufferSize, flags),
+		D3D12_RESOURCE_STATE_COMMON /*D3D12_RESOURCE_STATE_COPY_DEST*/,
+		nullptr,
+		IID_PPV_ARGS(pDestinationResource)));
+
+	// Create a committed resource for the upload.
+	if (bufferData)
+	{
+		ThrowIfFailed(device->CreateCommittedResource(
+			&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
+			D3D12_HEAP_FLAG_NONE,
+			&CD3DX12_RESOURCE_DESC::Buffer(bufferSize),
+			D3D12_RESOURCE_STATE_GENERIC_READ,
+			nullptr,
+			IID_PPV_ARGS(pIntermediateResource)));
+
+		D3D12_SUBRESOURCE_DATA subresourceData = {};
+		subresourceData.pData = bufferData;
+		subresourceData.RowPitch = bufferSize;
+		subresourceData.SlicePitch = subresourceData.RowPitch;
+
+		UpdateSubresources(commandList.Get(),
+			*pDestinationResource, *pIntermediateResource,
+			0, 0, 1, &subresourceData);
+	}
+}
+
+
+bool Sample0::LoadContent()
+{
+	std::wstring shaderBlobPath = GetExePath();
+	assert(shaderBlobPath.size() != 0);
+
+	auto device = Application::GetDevice();
+	auto commandQueue = Application::GetCommandQueue(D3D12_COMMAND_LIST_TYPE_COPY);
+	auto commandList = commandQueue->GetCommandList();
+
+	// Upload vertex buffer data.
+	ComPtr<ID3D12Resource> intermediateVertexBuffer;
+	UpdateBufferResource(commandList,
+		&m_VertexBuffer, &intermediateVertexBuffer,
+		_countof(g_Vertices), sizeof(VertexPosColor), g_Vertices);
+
+	// Create the vertex buffer view.
+	m_VertexBufferView.BufferLocation = m_VertexBuffer->GetGPUVirtualAddress();
+	m_VertexBufferView.SizeInBytes = sizeof(g_Vertices);
+	m_VertexBufferView.StrideInBytes = sizeof(VertexPosColor);
+
+	// Upload index buffer data.
+	ComPtr<ID3D12Resource> intermediateIndexBuffer;
+	UpdateBufferResource(commandList,
+		&m_IndexBuffer, &intermediateIndexBuffer,
+		_countof(g_Indicies), sizeof(WORD), g_Indicies);
+
+	// Create index buffer view.
+	m_IndexBufferView.BufferLocation = m_IndexBuffer->GetGPUVirtualAddress();
+	m_IndexBufferView.Format = DXGI_FORMAT_R16_UINT;
+	m_IndexBufferView.SizeInBytes = sizeof(g_Indicies);
+
+	// Create the descriptor heap for the depth-stencil view.
+	D3D12_DESCRIPTOR_HEAP_DESC dsvHeapDesc = {};
+	dsvHeapDesc.NumDescriptors = 1;
+	dsvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_DSV;
+	dsvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+	ThrowIfFailed(device->CreateDescriptorHeap(&dsvHeapDesc, IID_PPV_ARGS(&m_DSVHeap)));
+
+	// Load the vertex shader.
+	ComPtr<ID3DBlob> vertexShaderBlob;
+	std::wstring blobPath = std::wstring(shaderBlobPath + L"VertexShader.cso");
+	ThrowIfFailed(D3DReadFileToBlob(blobPath.c_str(), &vertexShaderBlob));
+
+	// Load the pixel shader.
+	ComPtr<ID3DBlob> pixelShaderBlob;
+	blobPath = std::wstring(shaderBlobPath + L"PixelShader.cso");
+	ThrowIfFailed(D3DReadFileToBlob(blobPath.c_str(), &pixelShaderBlob));
+
+	// Create the vertex input layout
+	D3D12_INPUT_ELEMENT_DESC inputLayout[] = {
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+		{ "COLOR", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+	};
+
+	// Create a root signature.
+	D3D12_FEATURE_DATA_ROOT_SIGNATURE featureData = {};
+	featureData.HighestVersion = D3D_ROOT_SIGNATURE_VERSION_1_1;
+	if (FAILED(device->CheckFeatureSupport(D3D12_FEATURE_ROOT_SIGNATURE, &featureData, sizeof(featureData))))
+	{
+		featureData.HighestVersion = D3D_ROOT_SIGNATURE_VERSION_1_0;
+	}
+
+	// Allow input layout and deny unnecessary access to certain pipeline stages.
+	D3D12_ROOT_SIGNATURE_FLAGS rootSignatureFlags =
+		D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT |
+		D3D12_ROOT_SIGNATURE_FLAG_DENY_HULL_SHADER_ROOT_ACCESS |
+		D3D12_ROOT_SIGNATURE_FLAG_DENY_DOMAIN_SHADER_ROOT_ACCESS |
+		D3D12_ROOT_SIGNATURE_FLAG_DENY_GEOMETRY_SHADER_ROOT_ACCESS |
+		D3D12_ROOT_SIGNATURE_FLAG_DENY_PIXEL_SHADER_ROOT_ACCESS;
+
+	// A single 32-bit constant root parameter that is used by the vertex shader.
+	CD3DX12_ROOT_PARAMETER1 rootParameters[1];
+	rootParameters[0].InitAsConstants(sizeof(XMMATRIX) / 4, 0, 0, D3D12_SHADER_VISIBILITY_VERTEX);
+
+	CD3DX12_VERSIONED_ROOT_SIGNATURE_DESC rootSignatureDescription;
+	rootSignatureDescription.Init_1_1(_countof(rootParameters), rootParameters, 0, nullptr, rootSignatureFlags);
+
+	// Serialize the root signature.
+	ComPtr<ID3DBlob> rootSignatureBlob;
+	ComPtr<ID3DBlob> errorBlob;
+	ThrowIfFailed(D3DX12SerializeVersionedRootSignature(&rootSignatureDescription,
+		featureData.HighestVersion, &rootSignatureBlob, &errorBlob));
+	// Create the root signature.
+	ThrowIfFailed(device->CreateRootSignature(0, rootSignatureBlob->GetBufferPointer(),
+		rootSignatureBlob->GetBufferSize(), IID_PPV_ARGS(&m_RootSignature)));
+
+	struct PipelineStateStream
+	{
+		CD3DX12_PIPELINE_STATE_STREAM_ROOT_SIGNATURE pRootSignature;
+		CD3DX12_PIPELINE_STATE_STREAM_INPUT_LAYOUT InputLayout;
+		CD3DX12_PIPELINE_STATE_STREAM_PRIMITIVE_TOPOLOGY PrimitiveTopologyType;
+		CD3DX12_PIPELINE_STATE_STREAM_VS VS;
+		CD3DX12_PIPELINE_STATE_STREAM_PS PS;
+		CD3DX12_PIPELINE_STATE_STREAM_DEPTH_STENCIL_FORMAT DSVFormat;
+		CD3DX12_PIPELINE_STATE_STREAM_RENDER_TARGET_FORMATS RTVFormats;
+	} pipelineStateStream;
+
+	D3D12_RT_FORMAT_ARRAY rtvFormats = {};
+	rtvFormats.NumRenderTargets = 1;
+	rtvFormats.RTFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
+
+	pipelineStateStream.pRootSignature = m_RootSignature.Get();
+	pipelineStateStream.InputLayout = { inputLayout, _countof(inputLayout) };
+	pipelineStateStream.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+	pipelineStateStream.VS = CD3DX12_SHADER_BYTECODE(vertexShaderBlob.Get());
+	pipelineStateStream.PS = CD3DX12_SHADER_BYTECODE(pixelShaderBlob.Get());
+	pipelineStateStream.DSVFormat = DXGI_FORMAT_D32_FLOAT;
+	pipelineStateStream.RTVFormats = rtvFormats;
+
+	D3D12_PIPELINE_STATE_STREAM_DESC pipelineStateStreamDesc = {
+		sizeof(PipelineStateStream), &pipelineStateStream
+	};
+	ThrowIfFailed(device->CreatePipelineState(&pipelineStateStreamDesc, IID_PPV_ARGS(&m_PipelineState)));
+
+	auto fenceValue = commandQueue->ExecuteCommandList(commandList);
+	commandQueue->WaitForFenceValue(fenceValue);
+
+	m_ContentLoaded = true;
+
+	// Resize/Create the depth buffer.
+	ResizeDepthBuffer(Application::GetClientWidth(), Application::GetClientHeight());
+
+	return true;
+}
+
+
+void Sample0::UnloadContent() {
+	m_ContentLoaded = false;
+}
+
+
+// =====================================================================================
+//							  Update & Render & Resize
+// =====================================================================================
+
+
+void Sample0::Update()
+{ 
+	Application::Update(); 
+	double totalUpdateTime = Application::GetUpdateTotalTime();
+
+	// Update the model matrix.
+	float angle = static_cast<float>(totalUpdateTime * 90.0);
+	const XMVECTOR rotationAxis = XMVectorSet(0, 1, 1, 0);
+	m_ModelMatrix = XMMatrixRotationAxis(rotationAxis, XMConvertToRadians(angle));
+
+	// Update the view matrix.
+	const XMVECTOR eyePosition = XMVectorSet(0, 0, -10, 1);
+	const XMVECTOR focusPoint = XMVectorSet(0, 0, 0, 1);
+	const XMVECTOR upDirection = XMVectorSet(0, 1, 0, 0);
+	m_ViewMatrix = XMMatrixLookAtLH(eyePosition, focusPoint, upDirection);
+
+	// Update the projection matrix.
+	float aspectRatio = Application::GetClientWidth() / static_cast<float>(Application::GetClientHeight());
+	m_ProjectionMatrix = XMMatrixPerspectiveFovLH(XMConvertToRadians(m_FoV), aspectRatio, 0.1f, 100.0f);
+}
+
+// Resources must be transitioned from one state to another using a resource BARRIER
+//		and inserting that resource barrier into the command list.
+// For example, before you can use the swap chain's back buffer as a render target, 
+//		it must be transitioned to the RENDER_TARGET state and before it can be used
+//		for presenting the rendered image to the screen, it must be transitioned to 
+//		the  PRESENT state.
+// 
+//
+// There are several types of resource barriers :
+//	 - Transition: Transitions a(sub)resource to a particular state before using it. 
+//			For example, before a texture can be used in a pixel shader, it must be 
+//			transitioned to the PIXEL_SHADER_RESOURCE state.
+//	 - Aliasing: Specifies that a resource is used in a placed or reserved heap when
+//			that resource is aliased with another resource in the same heap.
+//	 - UAV: Indicates that all UAV accesses to a particular resource have completed 
+//			before any future UAV access can begin.This is necessary when the UAV is 
+//			transitioned for :
+//				* Read > Write: Guarantees that all previous read operations on the UAV
+//						have completed before being written to in another shader.
+//				* Write > Read: Guarantees that all previous write operations on the UAV
+//						have completed before being read from in another shader.
+//				* Write > Write: Avoids race conditions that could be caused by different
+//						shaders in a different draw or dispatch trying to write to the same
+//						resource(does not avoid race conditions that could be caused in the
+//						same draw or dispatch call).
+//				* A UAV barrier is not needed if the resource is being used as a 
+//						read - only(Read > Read) resource between draw or dispatches.
+void Sample0::Render()
+{
+	Application::Render();
+	double totalRenderTime = Application::GetRenderTotalTime();
+
+	auto commandQueue = Application::GetCommandQueue(D3D12_COMMAND_LIST_TYPE_DIRECT);
+	auto commandList = commandQueue->GetCommandList();
+	
+	m_CurrentBackBufferIndex = Application::GetCurrentBackbufferIndex();
+	auto backBuffer = Application::GetBackbuffer(m_CurrentBackBufferIndex);
+
+	auto rtv = Application::GetCurrentBackbufferRTV();
+	auto dsv = m_DSVHeap->GetCPUDescriptorHandleForHeapStart();
+
+	// Clear RT
+	{
+		TransitionResource(commandList, backBuffer, D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET);
+
+		FLOAT clearColor[] = { 0.4f, 0.6f, 0.9f, 1.0f };
+		ClearRTV(commandList, rtv, clearColor);
+		ClearDepth(commandList, dsv);
+	}
+
+	// Set Graphics state
+	commandList->SetPipelineState(m_PipelineState.Get());
+	commandList->SetGraphicsRootSignature(m_RootSignature.Get());
+
+	commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	commandList->IASetVertexBuffers(0, 1, &m_VertexBufferView);
+	commandList->IASetIndexBuffer(&m_IndexBufferView);
+
+	commandList->RSSetViewports(1, &m_Viewport);
+	commandList->RSSetScissorRects(1, &m_ScissorRect);
+
+	commandList->OMSetRenderTargets(1, &rtv, FALSE, &dsv);
+
+	// Update the MVP matrix
+	XMMATRIX mvpMatrix = XMMatrixMultiply(m_ModelMatrix, m_ViewMatrix);
+	mvpMatrix = XMMatrixMultiply(mvpMatrix, m_ProjectionMatrix);
+	commandList->SetGraphicsRoot32BitConstants(0, sizeof(XMMATRIX) / 4, &mvpMatrix, 0);
+
+	// Draw
+	commandList->DrawIndexedInstanced(_countof(g_Indicies), 1, 0, 0, 0);
+
+	// PRESENT image
+	{
+		// After rendering the scene, the current back buffer is PRESENTed 
+		//     to the screen.
+		// !!! Before presenting, the back buffer resource must be 
+		//     transitioned to the PRESENT state.
+		TransitionResource(commandList, backBuffer, D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT);
+
+		// Execute
+		m_FenceValues[m_CurrentBackBufferIndex] = commandQueue->ExecuteCommandList(commandList);
+
+		m_CurrentBackBufferIndex = Application::Present();
+		commandQueue->WaitForFenceValue(m_FenceValues[m_CurrentBackBufferIndex]);
+	}
+}
+
+
+void Sample0::Resize(UINT32 width, UINT32 height)
+{
+	if (Application::GetClientWidth() != width || Application::GetClientHeight() != height)
+	{
+		// RenderTargets
+		{
+			Application::Resize(width, height);
+
+			// Since the index of back buffer may not be the same, it is important
+			// to update the current back buffer index as known by the application.
+			m_CurrentBackBufferIndex = Application::GetCurrentBackbufferIndex();
+		}
+
+		// Viewport and DephBuffer
+		{
+			m_Viewport = CD3DX12_VIEWPORT(0.0f, 0.0f,
+				static_cast<float>(width), static_cast<float>(height));
+
+			ResizeDepthBuffer(width, height);
+		}
+	}
+}
+
+
+void Sample0::ResizeDepthBuffer(int width, int height)
+{
+	if (m_ContentLoaded)
+	{
+		// Flush any GPU commands that might be referencing the depth buffer.
+		Application::Flush();
+
+		width = std::max(1, width);
+		height = std::max(1, height);
+
+		auto device = Application::GetDevice();
+
+		// Resize screen dependent resources.
+		// Create a depth buffer.
+		D3D12_CLEAR_VALUE optimizedClearValue = {};
+		optimizedClearValue.Format = DXGI_FORMAT_D32_FLOAT;
+		optimizedClearValue.DepthStencil = { 1.0f, 0 };
+
+		ThrowIfFailed(device->CreateCommittedResource(
+			&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+			D3D12_HEAP_FLAG_NONE,
+			&CD3DX12_RESOURCE_DESC::Tex2D(DXGI_FORMAT_D32_FLOAT, width, height,
+				1, 0, 1, 0, D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL),
+			D3D12_RESOURCE_STATE_DEPTH_WRITE,
+			&optimizedClearValue,
+			IID_PPV_ARGS(&m_DepthBuffer)
+		));
+
+		// Update the depth-stencil view.
+		D3D12_DEPTH_STENCIL_VIEW_DESC dsv = {};
+		dsv.Format = DXGI_FORMAT_D32_FLOAT;
+		dsv.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2D;
+		dsv.Texture2D.MipSlice = 0;
+		dsv.Flags = D3D12_DSV_FLAG_NONE;
+
+		device->CreateDepthStencilView(m_DepthBuffer.Get(), &dsv,
+			m_DSVHeap->GetCPUDescriptorHandleForHeapStart());
+	}
+}
+
+
+// =====================================================================================
+//									Helper Funcs
+// =====================================================================================
+
+
+void Sample0::TransitionResource(ComPtr<ID3D12GraphicsCommandList2> commandList, ComPtr<ID3D12Resource> resource,
+	D3D12_RESOURCE_STATES before, D3D12_RESOURCE_STATES after)
+{
+	CD3DX12_RESOURCE_BARRIER barrier = CD3DX12_RESOURCE_BARRIER::Transition
+	(
+		resource.Get(), before, after
+	);
+
+	commandList->ResourceBarrier(1, &barrier);
+}
+
+
+void Sample0::ClearRTV(Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList2> commandList,
+	D3D12_CPU_DESCRIPTOR_HANDLE rtv, FLOAT* clearColor)
+{
+	commandList->ClearRenderTargetView(rtv, clearColor, 0, nullptr);
+}
+
+
+void Sample0::ClearDepth(Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList2> commandList,
+	D3D12_CPU_DESCRIPTOR_HANDLE dsv, FLOAT depth)
+{
+	commandList->ClearDepthStencilView(dsv, D3D12_CLEAR_FLAG_DEPTH, depth, 0, 0, nullptr);
+}
