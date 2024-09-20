4c7153958f5a74ffae60575a9b8199a32357eb38
ferid <farik69rzaev@gmail.com>
2024-09-20 23:38:32 +0100

    Creating a 2nd project.

---
+++ Samples/01_Cube__FrameworkFull/Src/Sample1.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include "Framework/Game.h"
+
+#include <DirectXMath.h>
+
+
+class Sample1 : public Game
+{
+// ------------------------------------------------------------------------------------------
+//									Function members
+// ------------------------------------------------------------------------------------------
+public: // STATIC
+#if 0
+	static void Create(HINSTANCE hInstance, const wchar_t* windowTitle, int width, int height, bool vSync);
+	static void Destroy();
+	static Sample1& Get();
+#endif
+
+public:
+	// Init 
+	Sample1(HINSTANCE hInstance);
+	bool Initialize(const wchar_t* windowTitle, int width, int height, bool vSync);
+	virtual ~Sample1();
+
+	// Run
+	virtual int Run() final;
+
+protected:
+	virtual void Update() final;
+	virtual void Render() final;
+	virtual void Resize(UINT32 width, UINT32 height) final;
+
+	// Sample
+	bool LoadContent();
+	void UnloadContent();
+
+protected:
+	// Create a GPU buffer.
+	void UpdateBufferResource(ComPtr<ID3D12GraphicsCommandList2> commandList,
+		ID3D12Resource** pDestinationResource, ID3D12Resource** pIntermediateResource,
+		size_t numElements, size_t elementSize, const void* bufferData,
+		D3D12_RESOURCE_FLAGS flags = D3D12_RESOURCE_FLAG_NONE);
+	// Resize the depth buffer to match the size of the client area.
+	void ResizeDepthBuffer(int width, int height);
+
+	// Helpers
+	void TransitionResource(ComPtr<ID3D12GraphicsCommandList2> commandList, ComPtr<ID3D12Resource> resource, 
+		D3D12_RESOURCE_STATES before, D3D12_RESOURCE_STATES after);
+	void ClearRTV(Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList2> commandList,
+		D3D12_CPU_DESCRIPTOR_HANDLE rtv, FLOAT* clearColor);
+	void ClearDepth(Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList2> commandList,
+		D3D12_CPU_DESCRIPTOR_HANDLE dsv, FLOAT depth = 1.0f);
+
+// ------------------------------------------------------------------------------------------
+//									Data members
+// ------------------------------------------------------------------------------------------
+private:
+	bool m_ContentLoaded;
+
+	// Vertex buffer for the cube.
+	ComPtr<ID3D12Resource> m_VertexBuffer;
+	D3D12_VERTEX_BUFFER_VIEW m_VertexBufferView;
+	// Index buffer for the cube.
+	ComPtr<ID3D12Resource> m_IndexBuffer;
+	D3D12_INDEX_BUFFER_VIEW m_IndexBufferView;
+
+	// Depth buffer.
+	ComPtr<ID3D12Resource> m_DepthBuffer;
+	// Descriptor heap for depth buffer.
+	ComPtr<ID3D12DescriptorHeap> m_DSVHeap;
+
+	// Root signature
+	ComPtr<ID3D12RootSignature> m_RootSignature;
+
+	// Pipeline state object.
+	ComPtr<ID3D12PipelineState> m_PipelineState;
+private:	
+	// View Settings
+	D3D12_VIEWPORT m_Viewport = {};
+	D3D12_RECT m_ScissorRect = {};
+	float m_FoV;
+
+	// Camera
+	DirectX::XMMATRIX m_ModelMatrix;
+	DirectX::XMMATRIX m_ViewMatrix;
+	DirectX::XMMATRIX m_ProjectionMatrix;
+
+private:	
+	// Frame Sync 
+	UINT m_CurrentBackBufferIndex;
+	UINT64 m_FenceValues[NUM_FRAMES_IN_FLIGHT] = {};
+};
---
+++ Samples/01_Cube__FrameworkFull/Src/main.cpp
@@ -0,0 +1,40 @@
+// This is the exact copy of the 00_Cube project but hooked up to the FULL (not simplified) 
+// version of the framework DX12_FW.
+
+#include "Sample1.h"
+
+#include <dxgidebug.h>
+
+void ReportLiveObjects()
+{
+	IDXGIDebug1* dxgiDebug;
+	DXGIGetDebugInterface1(0, IID_PPV_ARGS(&dxgiDebug));
+
+	dxgiDebug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_IGNORE_INTERNAL);
+	dxgiDebug->Release();
+}
+
+int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow)
+{
+	const wchar_t* windowTitle = L"Learning DirectX 12";
+	int retCode = 0;
+
+#if 0
+	Sample1::Create(hInstance, windowTitle, 1280, 720, false);
+	{
+		Sample1::Get().Run();
+	}
+	Sample1::Destroy();
+#else
+	Sample1 sample(hInstance);
+	sample.Initialize(windowTitle, 1280, 720, false);
+	{
+		retCode = sample.Run();
+	}
+#endif
+
+	// ReportLiveObjects
+	atexit(&ReportLiveObjects);
+
+	return retCode;
+}
