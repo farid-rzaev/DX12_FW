c946fa92a784703acdf229b67c2263095e1e122c
ferid <farik69rzaev@gmail.com>
2025-06-30 09:03:22 +0100

    [BROKEN] ConnandQueue: Adding a second thread for CommandLists recycling
    1. Removing handling Command Allocators separately from Command Lists;
    2. Adding a second thread to recycle Command Lists;

--- Framework/CommandQueue.h
+++ Framework/CommandQueue.h
@@ -6,12 +6,10 @@
 #include <memory>	// shared_ptr
 #include <queue>
 
-
-using Microsoft::WRL::ComPtr;
-
+#include <External/Threading/ThreadSafeQueue.h>
 
 class Application;
-
+class CommandList;
 
 class CommandQueue
 {
@@ -24,51 +22,48 @@ public:
 	void WaitForFenceValue(uint64_t fenceValue);
 	void Flush();
 
+	// Wait for another command queue to finish.
+	void Wait(const CommandQueue& other);
+
 	// Returns the fence value to wait for this command list.
-	uint64_t ExecuteCommandList(ComPtr<ID3D12GraphicsCommandList2> commandList);
+	uint64_t ExecuteCommandList(std::shared_ptr<CommandList> commandList);
+	uint64_t ExecuteCommandLists(const std::vector<std::shared_ptr<CommandList> >& commandLists);
 	
 	// Get an available command list from the command queue.
-	ComPtr<ID3D12GraphicsCommandList2> GetCommandList();
-	ComPtr<ID3D12CommandQueue> GetD3D12CommandQueue() const;
-
-protected:
-	ComPtr<ID3D12CommandAllocator> CreateCommandAllocator();
-	ComPtr<ID3D12GraphicsCommandList2> CreateCommandList(ComPtr<ID3D12CommandAllocator> allocator);
+	std::shared_ptr<CommandList> GetCommandList();
+	Microsoft::WRL::ComPtr<ID3D12CommandQueue> GetD3D12CommandQueue() const;
 
 private /*helpers*/:
-	// Keep track of command allocators that are "in-flight":
-	//		Struct is used to associate a fence value with a command allocator.
-	//		It is known that - the command list can be reused immediately after it has been executed
-	//		on the command queue, but the command allocator cannot be reused unless the commands 
-	//		stored in the command allocator have finished executing on the command queue.
-	//		To guarantee that the command allocator is no longer “in-flight” on the command queue, 
-	//		a fence value is signaled on the command queue and that fence value 
-	//		(together with the associated command allocator) is stored for later reuse.
-	struct CommandAllocatorEntry
-	{
-		uint64_t fenceValue;
-		ComPtr<ID3D12CommandAllocator> commandAllocator;
-	};
 
-	using CommandAllocatorQueue = std::queue<CommandAllocatorEntry>;
-	using CommandListQueue = std::queue< ComPtr<ID3D12GraphicsCommandList2> >;
+	// Free any command lists that are finished processing on the command queue.
+	void ProccessInFlightCommandLists();
+
+	// Keep track of command allocators that are "in-flight"
+	// The first member is the fence value to wait for, the second is the 
+	// a shared pointer to the "in-flight" command list.
+	using CommandListEntry = std::tuple<uint64_t, std::shared_ptr<CommandList>>;
 
 private /*main*/:
-	// There is no need to associate a fence value with the command lists since 
-	// they can be reused right after they have been executed on the command queue.
-	CommandAllocatorQueue			m_CommandAllocatorQueue;
-	CommandListQueue				m_CommandListQueue;
 
 	// CommandQueue
-	D3D12_COMMAND_LIST_TYPE			m_CommandListType;
-	ComPtr<ID3D12CommandQueue>		m_d3d12CommandQueue;
+	D3D12_COMMAND_LIST_TYPE							m_CommandListType;
+	Microsoft::WRL::ComPtr<ID3D12CommandQueue>		m_d3d12CommandQueue;
 
 	// Synchronization objects
-	ComPtr<ID3D12Fence>				m_d3d12Fence;
-	HANDLE							m_FenceEvent;
-	uint64_t						m_FenceValue = 0;
+	Microsoft::WRL::ComPtr<ID3D12Fence>				m_d3d12Fence;
+	std::atomic_uint64_t							m_FenceValue = 0;
+
+	// Command Lists
+	ThreadSafeQueue<CommandListEntry>				m_InFlightCommandLists;
+	ThreadSafeQueue<std::shared_ptr<CommandList>>	m_AvailableCommandLists;
+
+	// A thread to process in-flight command lists.
+	std::thread										m_ProcessInFlightCommandListsThread;
+	std::atomic_bool								m_bProcessInFlightCommandLists;
+	std::mutex										m_ProcessInFlightCommandListsThreadMutex;
+	std::condition_variable							m_ProcessInFlightCommandListsThreadCV;
 
 	// Application
-	std::shared_ptr<Application>	m_application;
+	std::shared_ptr<Application>					m_application;
 };
 
